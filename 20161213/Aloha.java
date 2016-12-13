e99191e6ad5cacdc5c1a0edfd2c93053f5b32a13
maedatoma <maeda_toma@yse-c.net>
2016-12-13 14:36:05 +0900

    Aloha.java fixed.

--- 20161213/Aloha.java
+++ 20161213/Aloha.java
@@ -1,13 +1,26 @@
 public class Aloha{
 	public static void main(String[] args) {
 
-		// 1.
+		// Data
+		System.out.println("1st >");
+		String mainMsg = new java.util.Scanner(System.in).nextLine();
+
+		System.out.println("2nd >");
+		String subMsg = new java.util.Scanner(System.in).nextLine();
+
+		// method
 		for( int i=0; i<20; i++ ){
+
+			// 3回に1回
 			if ( i%3 == 0 ){
 				System.out.println(i + ": " + args[0] );
 		} else {
 				System.out.println(i+": " + args[1] );
 			}
 		}
+
+		// String の結果を表示する
+		System.out.println( mainMsg );
+		System.out.println( subMsg );
 	}
 }
