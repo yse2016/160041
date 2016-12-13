91dbc86d51d52f7a986cc1fad09916592084362a
maedatoma <maeda_toma@yse-c.net>
2016-12-13 14:46:51 +0900

    Aloha.java fixed.

--- 20161213/Aloha.java
+++ 20161213/Aloha.java
@@ -2,25 +2,34 @@ public class Aloha{
 	public static void main(String[] args) {
 
 		// Data
-		System.out.println("1st >");
-		String mainMsg = new java.util.Scanner(System.in).nextLine();
-
-		System.out.println("2nd >");
-		String subMsg = new java.util.Scanner(System.in).nextLine();
+		String mainMsg;
 
 		// method
 		for( int i=0; i<20; i++ ){
 
 			// 3回に1回
-			if ( i%3 == 0 ){
-				System.out.println(i + ": " + args[0] );
-		} else {
-				System.out.println(i+": " + args[1] );
+		//	if ( i%3 == 0 ){
+		//		System.out.println(i + ": " + args[0] );
+		//} else {
+		//		System.out.println(i+": " + args[1] );
+		//	}
+		}
+
+		// 繰り返しの処理
+		while(true){
+			System.out.println("exitを入力するまで終了できません >");
+			mainMsg = new java.util.Scanner(System.in).nextLine();
+
+			// 繰り返し終了の処理
+			if( mainMsg.equals("exit") ){
+				break;
 			}
 		}
 
-		// String の結果を表示する
-		System.out.println( mainMsg );
-		System.out.println( subMsg );
+		//System.out.println("2nd >");
+		//String subMsg = new java.util.Scanner(System.in).nextLine();
+
+		// 繰り返しの脱出結果を表示する
+		System.out.println( "end." );
 	}
 }
