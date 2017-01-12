74a879c69030924038020238aa12970e65428aa9
maedatoma <maeda_toma@yse-c.net>
2017-01-12 16:08:24 +0900

    added 20170112

--- 20170110/Aloha.java
+++ 20170110/Aloha.java
@@ -19,7 +19,25 @@ public class Aloha {
 
 		// 3.
 		Alohaman am = new Alohaman();
-		am.sayAloha(20,3);
+
+		// args[]の個数によって、使い分ける
+		int num = args.length;
+
+		// args[]の個数による場合分け
+		if(num == 0) {
+			am.sayAloha();
+
+		} else if( num == 1 ) {
+			// args[]は文字列なので、int に変換する
+			int data1 = Integer.parseInt( args[0] );
+			am.sayAloha( data1 );
+
+		} else if( num == 2 ) {
+			// args[]は文字列なので、int に変換する
+			int data1 = Integer.parseInt( args[0] );
+			int data2 = Integer.parseInt( args[1] );
+			am.sayAloha( data1, data2 );
+		}
 	}
 }
 
@@ -39,21 +57,21 @@ class Alohaman {
 	}
 
 	public void sayAloha(){
-		// "Aloha"と出力
+		// msg1 を出力
 		System.out.println( this.msg1 );
 	}
 
 	public void sayAloha( int n ){
-		// "Aloha"と n 回出力
+		// msg1 を n 回出力
 		for( int i=0; i<n; i++ ){
 			System.out.println( this.msg1 );
 		}
 	}
 
 	public void sayAloha( int n, int x ){
-		// "Aloha"と n 回出力し、x 回に1回、"Hello"と出力
+		// msg1 を n 回出力し、x 回に1回、msg2 を出力
 		for( int i=0; i<n; i++ ){
-			if( i%3 == 0 ){
+			if( i%x == 0 ){
 				System.out.println( i +": "+ msg2 );
 			} else {
 				System.out.println( i +": "+ msg1 );
