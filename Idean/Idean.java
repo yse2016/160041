e8712efc9c83cc6123c05ebd9d7d33b2eb9c5114
maedatoma <maeda_toma@yse-c.net>
2017-01-23 12:18:05 +0900

    added Textfield, Button etc...

--- Idean/Idean.java
+++ Idean/Idean.java
@@ -1,8 +1,11 @@
 // Idean.java
 
 /* 要件
-	- ボタン2つ
-	- アイデアを入力するテキストフィールド
+	- インタフェース
+		- ボタン
+			- 単語2個表示ボタン
+			- メモを保存するためのボタン
+		- 単語を表示するテキストフィールド( 2つ )
 */
 
 import java.awt.*;
@@ -18,48 +21,59 @@ public class Idean{
 
 
 
-class IdeanMan{
+class IdeanMan implements ActionListener{
 	// field
 	JFrame frame;
 	JTextField idea_Name1;
 	JTextField idea_Name2;
+	JTextField file_Name;
+	JTextArea memo_Area;
 	JPanel field_Panel;
-	JButton btn1;
-	JButton btn2;
-
+	JButton btn_Idea;
+	JButton btn_Save;
 
 	// method
 	public IdeanMan(){
 		// フレームを作る
 		frame = new JFrame("Idean");
 		frame.setLocation( 600, 300 );
-		frame.setSize( 300, 300 );
+		frame.setSize( 512, 512 );
 
 		frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
 
 		// テキストフィールド,パネル,ボタンを作る
+			// アイデア出力欄( idea_Name1, idea_Name2 )
+			idea_Name1 = new JTextField("ここにアイデアが出ます",15);
+			idea_Name2 = new JTextField("ここにアイデアが出ます",15);
 
-			// テキストフィールド( idea_Name1, idea_Name2 )
-			idea_Name1 = new JTextField("",10);
-			idea_Name2 = new JTextField("",10);
+			// メモ欄( memo_Area )
+			memo_Area = new JTextArea( 10, 30 );
+
+			// ファイル名入力欄
+			file_Name = new JTextField("ファイル名を入力.txt",15);
 
 			// パネル
 			field_Panel = new JPanel();
 
 			// ボタン
-				// btn1
-				btn1 = new JButton("btn1");
+				// btn_Idea
+				btn_Idea = new JButton("単語2個表示");
+				btn_Idea.addActionListener(this);
+				btn_Idea.setActionCommand("idea");
 
-				// btn2
-				btn2 = new JButton("btn2");
+				// 保存ボタン
+				btn_Save = new JButton("保存");
+				btn_Save.addActionListener(this);
+				btn_Save.setActionCommand("save");
 
 		// 載せる
-
 			// panel に field を載せる
 			field_Panel.add( idea_Name1 );
 			field_Panel.add( idea_Name2 );
-			field_Panel.add( btn1 );
-			field_Panel.add( btn2 );
+			field_Panel.add( btn_Idea );
+			field_Panel.add( memo_Area );
+			field_Panel.add( file_Name );
+			field_Panel.add( btn_Save );
 
 			// window に panel を載せる
 			Container con =frame.getContentPane();
@@ -68,6 +82,52 @@ class IdeanMan{
 
 		// window に表示
 		frame.setVisible(true);
+	}
 
+	public void actionPerformed( ActionEvent ae ){
+		// コマンドを調べる
+		String cmd = ae.getActionCommand();
+
+		// 各ボタンがクリックされた時の処理
+			// btn_Idea
+			if( cmd.equals("idea")){
+
+
+			// bnt_Save
+			} else if( cmd.equals("save")){
+				// ファイル名を調べる
+				String text_File_Name = file_Name.getText();
+
+				// データを準備する
+				FileWriter fw = null;
+				PrintWriter pw = null;
+
+				try{
+					// ファイルを開く
+					fw = new FileWriter( text_File_Name );
+					pw = new PrintWriter( fw );
+
+					// 2つのアイデアフィールドを読み取る
+					String data1 = idea_Name1.getText();
+					String data2 = idea_Name2.getText();
+					String data3 = memo_Area.getText();
+
+					// ファイルに書き込み、保存
+					pw.println( data1 +" + "+ data2 + memo_Area );
+
+				// 例外処理
+				} catch( IOException e ){
+					System.out.println("エラー");
+				} finally {
+					// ファイルを閉じる
+					try{
+						fw.close();
+						pw.close();
+					} catch( IOException e ){
+						System.out.println("エラー");
+					}
+				}
+
+			}
 	}
 }
